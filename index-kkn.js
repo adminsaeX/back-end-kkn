4d792bda8dbbf8da349465a2d9958bf0c969e627
adminsaeX <51604957+adminsaeX@users.noreply.github.com>
2020-08-03 01:42:15 +0700

    update

--- index-kkn.js
+++ index-kkn.js
@@ -1,4 +1,12 @@
+var admin = require("firebase-admin");
 
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./serviceAccountKey.json");
+
+// Initialize the app with a service account, granting admin privileges
+
+var time = new Date( new Date().getTime() + 7 * 3600 * 1000);
+var time_str = time.toString();
 var app = require('express')();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 9010;
@@ -6,12 +14,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	//databaseURL: "https://solarpump-ec15e.firebaseio.com/"
+  });
+const db = admin.firestore();  
+const kkn2_device = require("./kkn2_device.json");
+//console.log(kkn2_device);
+
 app.post('/data', function (req, res) {
-	var json = req.body;
+	var json_data = req.body;
+	json_data = {...json_data,date:time};
 	var device_id = String(req.body.dev_ID);
     res.send('200');
-	console.log(json);
-	console.log(device_id)
+	//console.log(json);
+	console.log(device_id);
+	
+var picked = kkn2_device.find(o => o.device_ID === device_id);
+if(picked){
+	db.collection("KKN22019_data").add(json_data);
+}
+else
+{
+	db.collection("KKN12019_data").add(json_data);
+}
+
+
 	
 });
 
